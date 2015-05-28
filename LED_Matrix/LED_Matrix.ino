ac08e0bd712f4552447e881a371a45ee14686e19
Tobias Nuß <tobias.nuss@iosb.fraunhofer.de>
2015-05-28 14:22:50 +0200

    initialisation

---
+++ LED_Matrix/LED_Matrix.ino
@@ -0,0 +1,20 @@
+/*
+ * LED_Matrix.ino
+ *
+ * Created: 5/28/2015 2:21:42 PM
+ * Author: tobnu
+ */ 
+
+void setup()
+{
+
+	  /* add setup code here, setup code runs once when the processor starts */
+
+}
+
+void loop()
+{
+
+	  /* add main program code here, this code starts again each time it ends */
+
+}
